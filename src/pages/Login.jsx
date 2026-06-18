d43667cadbf5acf97eaf1fbd7fc63808711f24de
yooon2744 <lark5614.naver.com@gmail.com>
2026-06-18 20:01:39 +0900

    Remove Kakao login, keep Google only

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -56,10 +56,6 @@ export default function Login() {
             <GoogleIcon />
             Google로 시작하기
           </button>
-          <button className="social-btn social-kakao" onClick={() => signInWithSocial("kakao")}>
-            <KakaoIcon />
-            카카오로 시작하기
-          </button>
         </div>
 
         <div className="login-divider">또는 이메일로</div>
@@ -110,14 +106,6 @@ function GoogleIcon() {
       <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/>
       <path fill="#FBBC05" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"/>
       <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/>
-    </svg>
-  );
-}
-
-function KakaoIcon() {
-  return (
-    <svg width="18" height="18" viewBox="0 0 24 24">
-      <path fill="#3C1E1E" d="M12 3C6.48 3 2 6.48 2 10.8c0 2.7 1.6 5.07 4 6.53L5 21l3.8-2.53c1.02.27 2.1.43 3.2.43 5.52 0 10-3.48 10-7.8S17.52 3 12 3z"/>
     </svg>
   );
 }
